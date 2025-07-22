9b0b631550750d497ca34ba6d08826d38fd3f611
muhib53 <muhibkhanwazir009@gmail.com>
2025-07-21 21:58:17 -0700

    this is my ml coding in scikit learn

---
+++ first.py
@@ -0,0 +1,30 @@
+# # name = 'muhib khan.'
+# # age = 18
+# # marks = 100
+# # gpa = 7.8
+
+# # print(name,age,marks,gpa)
+# # print(type(name))
+# # print(type(age))
+
+
+# name ='muhib'
+# age = 22
+# print('name',name)# advace fuctions
+# print(f'my naem is{name}and i am{age}years old')
+
+# name = input('apka nam kya hai')
+# print('welcome',name)
+
+# age = input('apka age kitna hai')
+# print('welcome',age)
+
+# age = input('enter your age')
+# print(type(age))
+
+# age = int(age)
+# print(age + 5)
+
+num1 = float(input('enter your first number'))
+num2 = float(input('enter your secand number'))
+num1+num2
